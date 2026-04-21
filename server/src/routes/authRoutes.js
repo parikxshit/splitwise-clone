b99a0cb124bb62f914610de9e8a173b93326595b
Parikshit Malik <parikshit.malik@thumbmunkeys.com>
2026-04-21 20:29:04 +0530

    added refresh routes, schema, service and controller

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { authController } = require('../controllers');
-const { validate, registerSchema, loginSchema } = require('../validators/auth.validator');
+const { validate, registerSchema, loginSchema, refreshSchema } = require('../validators/auth.validator');
+const { refresh } = require('../controllers/authController');
 
 // POST /api/auth/register
 router.post('/register', validate(registerSchema), authController.register);
 
 // POST /api/auth/login
 router.post('/login', validate(loginSchema), authController.login);
+
+//POST /api/auth/refersh
+router.post('/refresh', validate(refreshSchema), authController.refresh);
 
 module.exports = router;
