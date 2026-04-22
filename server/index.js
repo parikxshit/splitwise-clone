1e1ebea52f0a2ffea5b49ab464ec2363ed46fcee
Parikshit Malik <parikshit.malik@thumbmunkeys.com>
2026-04-23 02:40:42 +0530

    Registartion Page made with shadecn/ui and user registered successfully, only initialisation, need to complete full functionality

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ const apiRoutes = require('./src/routes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
